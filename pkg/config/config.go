f53b38cbc3427a7ecd72ab143434895a0cb02ed8
CoolHandDev <coolhanddev@gmail.com>
2016-10-21 18:18:08 -0400

    add code to handle config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,5 +1,12 @@
 package config
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
 //MssqlCn represents the connection string
 type MssqlCn struct {
 	UserName  string
@@ -11,6 +18,21 @@ type MssqlCn struct {
 	AppName   string
 }
 
+//PrjConfig represents the configuration from the json file
+type PrjConfig struct {
+	Name        string
+	Description string
+	PrjType     string
+	Version     string
+	Scripts     CfgScripts
+}
+
+//CfgScripts represents the list of schema and process scripts
+type CfgScripts struct {
+	Schema  []string
+	Process []string
+}
+
 //GetCnString returns a sql server connection string
 func GetCnString(c MssqlCn) string {
 	return "user id=" + c.UserName +
@@ -20,5 +42,28 @@ func GetCnString(c MssqlCn) string {
 		";port=" + c.Port +
 		";connection timeout=" + c.CnTimeout +
 		";app name=" + c.AppName
+
+}
+
+var wrkConfig PrjConfig
+
+//ReadConfig reads the config file based on location passed in
+func ReadConfig(f string) {
+	_, err := os.Stat(f)
+	if os.IsNotExist(err) {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+
+	configInMem, err := ioutil.ReadFile(f)
+	if err != nil {
+		fmt.Println("error reading configuration")
+	}
+
+	err = json.Unmarshal([]byte(configInMem), &wrkConfig)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(wrkConfig)
 
 }
