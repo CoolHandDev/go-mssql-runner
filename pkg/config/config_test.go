380a4387c9c6d2106b16b6798eb909f2307e057a
CoolHandDev <coolhanddev@gmail.com>
2016-11-08 15:14:08 -0500

    test(config): add unit test for reading config

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -1,15 +1,13 @@
 package config
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"reflect"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestMakeConnectionString(t *testing.T) {
+
 	Convey("Given that all connection values are set", t, func() {
 		cases := make(map[string]MssqlCn)
 		//Case 1
@@ -21,9 +19,10 @@ func TestMakeConnectionString(t *testing.T) {
 		//Case 3
 		cn3 := MssqlCn{UserName: "anotheruser", Password: "", Server: "anotherhost", Database: "anotherdatabase", Port: "1433", AppName: "another-app-name", CnTimeout: "900"}
 		cases["user id=anotheruser"+";password="+";server=anotherhost"+";database=anotherdatabase"+";port=1433"+";connection timeout=900"+";app name=another-app-name"] = cn3
-		Convey("When they are used to construct a string", func() {
 
-			Convey("The output should be a connection string consisting of all those values", func() {
+		Convey("When they are used to construct a connectrion string", func() {
+
+			Convey("The output should be a MSSQL connection string consisting of all those values", func() {
 				for k, v := range cases {
 					So(k, ShouldEqual, GetCnString(v))
 				}
@@ -34,28 +33,58 @@ func TestMakeConnectionString(t *testing.T) {
 	})
 }
 
-//TestReadConfig tests if the config file is being read and unmarshalled
-func TestReadConfig(t *testing.T) {
-	var result PrjConfig
-	var expected PrjConfig
+func TestLoadConfiguration(t *testing.T) {
 
-	expected.Name = "example project"
-	expected.Description = "description for project"
-	expected.Type = "data analysis"
-	expected.Version = "1.0.0"
-	expected.Scripts.Schema = []string{"/schema/schema1.sql", "/schema/schema2.sql"}
-	expected.Scripts.Process = []string{"/process/process1.sql", "/process/process2.sql"}
-	configInMem, err := ioutil.ReadFile("../../mssqlrun.conf.json")
-	if err != nil {
-		panic(err)
-	}
-	err = json.Unmarshal(configInMem, &result)
-	if !reflect.DeepEqual(expected, result) {
-		t.Errorf("expected %s but got %s", expected, result)
-	}
-}
+	Convey("Given that a configuration file is specified", t, func() {
+		var expected PrjConfig
 
-//TestGetSchemaScripts tests if list of scrips is returned
-func TestGetSchemaScripts(t *testing.T) {
+		expected.Name = "example project"
+		expected.Description = "description for project"
+		expected.Type = "data analysis"
+		expected.Version = "1.0.0"
+		expected.Scripts.Schema = []string{"/schema/schema1.sql", "/schema/schema2.sql"}
+		expected.Scripts.Process = []string{"/process/process1.sql", "/process/process2.sql"}
 
+		Convey("When it is loaded", func() {
+
+			ReadConfig("../../mssqlrun.conf.json")
+			result := wrkConfig
+			Convey("The values from the file should be available to the app", func() {
+				So(result, ShouldResemble, expected)
+			})
+
+			Convey("The schema array should contain schema file names", func() {
+				Convey("The first schema file should exist", func() {
+					So(result.Scripts.Schema[0], ShouldEqual, expected.Scripts.Schema[0])
+				})
+				Convey("The last schema file should exist", func() {
+					resultIdx := len(result.Scripts.Schema) - 1
+					expectedIdx := len(expected.Scripts.Schema) - 1
+					So(result.Scripts.Schema[resultIdx], ShouldEqual, expected.Scripts.Schema[expectedIdx])
+				})
+			})
+
+			Convey("The process array should contain process file names", func() {
+				Convey("The first process file should exist", func() {
+					So(result.Scripts.Process[0], ShouldEqual, expected.Scripts.Process[0])
+				})
+				Convey("The last process file should exist", func() {
+					resultIdx := len(result.Scripts.Process) - 1
+					expectedIdx := len(expected.Scripts.Process) - 1
+					So(result.Scripts.Process[resultIdx], ShouldEqual, expected.Scripts.Process[expectedIdx])
+				})
+			})
+
+			Convey("The config should return a list of scripts", func() {
+				Convey("A list of schema scripts should be returned", func() {
+					schemaList := GetSchemaScripts()
+					So(schemaList, ShouldNotBeEmpty)
+				})
+				Convey("A list of process scripts should be returned", func() {
+					processList := GetProcessScripts()
+					So(processList, ShouldNotBeEmpty)
+				})
+			})
+		})
+	})
 }
