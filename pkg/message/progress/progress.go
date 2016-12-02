971d6fe2c754929ae1d9bca9b4a9240414870aaa
CoolHandDev <coolhanddev@gmail.com>
2016-12-01 20:06:04 -0500

    chore(logging): tweak logging output
    Add pipe delimiter to make log easier to parse. Add detail to display
    what database is being used. Add detail to display which configuration
    file is being used.  Prettify lines by adding equal sign.

--- pkg/message/progress/progress.go
+++ pkg/message/progress/progress.go
@@ -7,5 +7,5 @@ import (
 
 //Prefix applies a basic time prefix to a message
 func Prefix(msg ...string) string {
-	return time.Now().Format("2006-01-02 T15:04:05") + ": " + strings.Join(msg, " ")
+	return time.Now().Format("2006-01-02 15:04:05") + "| " + strings.Join(msg, " ")
 }
