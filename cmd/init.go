3f2eedf4f500f42e2935f5a6e1988bd55df42faa
Coolhanddev <coolhanddev@gmail.com>
2017-12-29 13:19:51 -0500

    add init command

--- cmd/init.go
+++ cmd/init.go
@@ -15,26 +15,54 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 
+	"github.com/coolhanddev/go-mssql-runner/pkg/config"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
 // initCmd represents the init command
 var initCmd = &cobra.Command{
 	Use:   "init",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("init called")
-	},
+	Short: "create a mssql.conf.json file in current directory",
+	Long:  ``,
+	Run:   initProj,
 }
 
+func initProj(cmd *cobra.Command, args []string) {
+	log.SetFormatter(&log.TextFormatter{TimestampFormat: "01-02-2006 15:04:05", FullTimestamp: true})
+	createConfig()
+}
+
+func createConfig() {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(dir)
+	cfgFile := config.PrjConfig{
+		Name:        "place project name here",
+		Description: "place description here",
+		Type:        "place project type here",
+		Version:     "place version here",
+		Scripts: config.CfgScripts{
+			Schema:  []string{"schema-example-1.sql"},
+			Process: []string{"process-example-1.sql"},
+		},
+	}
+	b, _ := json.MarshalIndent(cfgFile, "", "    ")
+	err = ioutil.WriteFile("mssqlrun.conf.json", b, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Info("mssqlrun.conf.json created.")
+	fmt.Println(string(b))
+}
 func init() {
 	RootCmd.AddCommand(initCmd)
 
