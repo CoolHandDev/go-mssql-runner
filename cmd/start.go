19d8729b5114f67a2c86a22a97c1a6cbd6879c41
Coolhanddev <coolhanddev@gmail.com>
2018-01-14 09:19:08 -0500

    remove cobra doc

--- cmd/start.go
+++ cmd/start.go
@@ -38,15 +38,6 @@ var startCmd = &cobra.Command{
 func init() {
 	RootCmd.AddCommand(startCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// startCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// startCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	logLevelMsg := "Specifies level of verbosity for SQL log output. See start command help (above) for details"
 	startCmd.Flags().StringVarP(&configFile, "conf", "c", "", "The configuration file")
 	startCmd.Flags().StringVarP(&userName, "username", "u", "", "SQL Server user name. *Required")
