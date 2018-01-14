8bcbdba5884a304add9f703929801a4ae19aca32
Coolhanddev <coolhanddev@gmail.com>
2018-01-14 09:21:47 -0500

    remove cobra doc

--- cmd/root.go
+++ cmd/root.go
@@ -66,9 +66,6 @@ go-mssql-runner start -c /path/configFile.json -u sqlUserName -p sqlPassword -s 
 
 
 `,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command sets flags appropriately.
@@ -82,14 +79,6 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports Persistent Flags, which, if defined here,
-	// will be global for your application.
-
-	//RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.go-mssql-runner.yaml)")
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
--- cmd/version.go
+++ cmd/version.go
@@ -15,22 +15,10 @@ var versionCmd = &cobra.Command{
 	Short: "Show the version",
 	Long:  `Show the version`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: Work your own magic here
 		fmt.Println("go-sql-runner:", AppVersion)
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(versionCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// versionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
 }
